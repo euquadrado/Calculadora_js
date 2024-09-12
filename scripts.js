af6886d055568cdca69d0071d541176e8a4ee0ef
euquadrado <efq2005@gmail.com>
2024-09-12 14:29:47 -0300

    Calculadora

---
+++ scripts.js
@@ -0,0 +1,95 @@
+// Seleção dos elementos //
+const display = document.querySelector("#displayInput");
+const botaoIgual = document.querySelector(".igual");
+const botaoPonto = document.querySelector(".ponto");
+const botoesNumeros = document.querySelectorAll(".num");
+const botoesOperadores = document.querySelectorAll(".operador");
+
+// Variáveis globais //
+let operacaoAtual = "";
+let operador = null;
+let valorAnterior = "";
+let calculando = false;
+
+// Funções //
+const atualizaDisplay = () => {
+  display.value = operacaoAtual;
+};
+
+const insereNumero = (evento) => {
+  if (calculando) {
+    operacaoAtual = evento.target.textContent;
+    calculando = false;
+  } else {
+    operacaoAtual += evento.target.textContent;
+  }
+  atualizaDisplay();
+};
+
+const insereOperador = (evento) => {
+  if (operacaoAtual !== "") {
+    if (!calculando) {
+      if (operador !== null) calcula();
+      valorAnterior = operacaoAtual;
+      operacaoAtual = "";
+    }
+    operador = evento.target.textContent;
+  }
+};
+// Operações //
+const calcula = () => {
+  let resultado = null;
+  const operandoAnterior = parseFloat(valorAnterior);
+  const operandoAtual = parseFloat(operacaoAtual);
+
+  switch (operador) {
+    case "+":
+      resultado = operandoAnterior + operandoAtual;
+      break;
+    case "-":
+      resultado = operandoAnterior - operandoAtual;
+      break;
+    case "x":
+      resultado = operandoAnterior * operandoAtual;
+      break;
+    case "/":
+      if (operandoAtual !== 0) {
+        resultado = operandoAnterior / operandoAtual;
+      } else {
+        alert("Erro: Divisão por zero não é permitida!");
+        return;
+      }
+      break;
+      case "%":
+      resultado = operandoAnterior * operandoAtual/ 100;
+      break;
+      
+     
+        
+  }
+  operacaoAtual = String(resultado);
+  valorAnterior = operacaoAtual;
+  calculando = true;
+  atualizaDisplay();
+};
+
+const inserePonto = () => {
+  if (operacaoAtual.indexOf(".") === -1) {
+    operacaoAtual += ".";
+    atualizaDisplay();
+  }
+};
+
+// EventoInternos //
+botaoIgual.addEventListener("click", () => {
+  if (operador !== null && operacaoAtual !== "" && !calculando) {
+    calcula();
+    operador = null;
+  }
+});
+botaoPonto.addEventListener("click", inserePonto);
+botoesNumeros.forEach((botao) => botao.addEventListener("click", insereNumero));
+botoesOperadores.forEach((botao) =>
+  botao.addEventListener("click", insereOperador)
+);
+
